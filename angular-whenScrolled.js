6325a06eb666bee20cfd820225004167360b9384
Juan David Parra <jdparrapimiento@gmail.com>
2014-10-27 14:28:30 -0500

    Create angular-whenScrolled.js

---
+++ angular-whenScrolled.js
@@ -0,0 +1,18 @@
+var mod;
+mod = angular.module('angular-whenScrolled', []);
+mod.directive("whenScrolled", function ($window) {
+    return{
+        restrict: 'A',
+        link: function (scope, elem, attrs) {
+            raw = elem[0];
+            var checkBounds = function (evt) {
+                var rectObject = raw.getBoundingClientRect();
+                if ($window.innerHeight >rectObject.bottom+100) {
+                    scope.loading = true;
+                    scope.$apply(attrs.whenScrolled);
+                }
+            };
+            angular.element($window).bind('scroll', checkBounds);
+        }
+    };
+});
